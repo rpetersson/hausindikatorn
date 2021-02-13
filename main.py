d957d441bdf64186a4da4c5116cf19c2760067da
rpetersson <rpetersson@gmail.com>
2021-02-13 11:51:06 +0100

    Uppdaterat

--- main.py
+++ main.py
@@ -1,20 +1,55 @@
 from textblob import TextBlob
-from googletrans import Translator, constants
-from pprint import pprint
+from googletrans import Translator
+from selenium import webdriver
+
+driver = webdriver.PhantomJS()
 
 translator = Translator()
 
-text = """Jeg hater deg din stygge fisk"""
+# URL
+side = 1
+max_side = str(10)
 
-translation = translator.translate(text)
+number_of_posts = 0
+sentiment = 0
+array_of_hauss = []
 
-text = translation.text
-print(text)
+while int(side) <= int(max_side):
+    synact = "https://www.avanza.se/placera/forum/forum/synact-pharma." + str(side) + ".html"
+    url = synact
+    driver.get(url)
+    print(url)
+    poster = driver.find_elements_by_class_name("userPost")
 
-blob = TextBlob(text)
+    for text in poster:
 
-for sentence in blob.sentences:
-    print(f"Sentiment: {sentence.sentiment.polarity}")
+        translation = translator.translate(text.text)
+        translated_text = translation.text
+        blob = TextBlob(translated_text)
+
+        for sentence in blob.sentences:
+            number_of_posts = number_of_posts + 1
+            #print(number_of_posts)
+            sentiment_per_post = sentence.sentiment.polarity
+            sentiment = sentiment + float(sentiment_per_post)
+            array_of_hauss.append(sentiment_per_post)
+    side = side + 1
+
+print("Antall poster: ", number_of_posts)
+print("Håsindex: ", sentiment)
+
+print("Genomsnitts hås", sentiment / number_of_posts)
+print(array_of_hauss)
+
+
+
+
+
+
+
+
+
+
 
 
 
