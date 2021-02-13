41b1bdcb9c2dcd9ad3b688d753ee76150342c69b
rpetersson <rpetersson@gmail.com>
2021-02-13 14:41:43 +0100

    Uppdaterat

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ driver = webdriver.PhantomJS()
 translator = Translator()
 
 # URL
-side = 1
-max_side = str(10)
+sidor = 1
+max_sidor = str(10)
 
 number_of_posts = 0
 sentiment = 0
-array_of_hauss = []
+list_of_hauss = []
 
-while int(side) <= int(max_side):
-    synact = "https://www.avanza.se/placera/forum/forum/synact-pharma." + str(side) + ".html"
+while int(sidor) <= int(max_sidor):
+    synact = "https://www.avanza.se/placera/forum/forum/synact-pharma." + str(sidor) + ".html"
     url = synact
     driver.get(url)
     print(url)
@@ -23,23 +23,22 @@ while int(side) <= int(max_side):
 
     for text in poster:
 
-        translation = translator.translate(text.text)
-        translated_text = translation.text
-        blob = TextBlob(translated_text)
+        translation = translator.translate(text.text)# Översätter med Google API
+        translated_text = translation.text # Lagrar översatt text i ny variabel.
+        blob = TextBlob(translated_text) # Tar den översatta texten och kör den i textBlob för att analysera.
+        sentiment_per_post = blob.sentiment[0] #Lagrar sentimentet i ny variabel. "fe.x: 0.001"
 
-        for sentence in blob.sentences:
-            number_of_posts = number_of_posts + 1
-            #print(number_of_posts)
-            sentiment_per_post = sentence.sentiment.polarity
-            sentiment = sentiment + float(sentiment_per_post)
-            array_of_hauss.append(sentiment_per_post)
-    side = side + 1
+        sentiment = sentiment + sentiment_per_post # Lägger ihop sentimentet för varje post.
+
+        number_of_posts = number_of_posts + 1 # Counter för antal poster den har analyserat.
+        list_of_hauss.append(sentiment_per_post)
+
+    sidor = sidor + 1
 
 print("Antall poster: ", number_of_posts)
 print("Håsindex: ", sentiment)
-
 print("Genomsnitts hås", sentiment / number_of_posts)
-print(array_of_hauss)
+print(list_of_hauss)
 
 
 
